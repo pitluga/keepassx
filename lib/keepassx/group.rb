5705965a292bdb37c776fb3db51246aa22a3091a
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2013-12-04 01:32:02 +0400

    Typo fix

--- lib/keepassx/group.rb
+++ lib/keepassx/group.rb
@@ -45,12 +45,8 @@ module Keepassx
       @fields.map(&:length).reduce(&:+)
     end
 
-    def group_id
-      @fields.detect { |field| field.name == 'groupid' }.data
-    end
-
     def method_missing(name)
-      @fields.detect { |field| field.name == 'groupid' }.data.chomp("\000")
+      @fields.detect { |field| field.name == name.to_s }.data.chomp("\000")
     end
   end
 end
